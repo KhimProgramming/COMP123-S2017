599fc5c564763d52fcc329bb1fc07fa354a0779f
Khim Tang <ktang33@my.centennialcollege.ca>
2017-06-29 15:36:55 -0400

    added some codes

--- ConsoleApplication1/Person.cs
+++ ConsoleApplication1/Person.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    /// <summary>
+    /// This is the Person Super Class
+    /// </summary>
     class Person
     {
+        //PRIVATE INSTANCE VARIABLES (FIELDS)
+        private string _name;
+        private int _age;
+
+        //
     }
 }
